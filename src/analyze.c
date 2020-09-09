07a268c6d5cd713b57ed18ac840d8f8173be983c
Riki Kyttälä <rkyttala@student.hive.fi>
2020-09-09 12:16:50 +0300

    git upload save

--- src/analyze.c
+++ src/analyze.c
@@ -6,21 +6,67 @@
 /*   By: rkyttala <rkyttala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 20:08:03 by rkyttala          #+#    #+#             */
-/*   Updated: 2020/09/09 10:20:44 by rkyttala         ###   ########.fr       */
+/*   Updated: 2020/09/09 12:11:14 by rkyttala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
 
-static int	read_token(t_game *game)
+/*
+** The format of the line is always "Plateau Y X\n" or "Piece Y X\n",
+** where Y is row count and X is column count, thus reading only the digits
+** from the line will get us our dimensions easily.
+*/
+int		get_dimensions(char *str, t_game *game, int board)
 {
+	int		i;
+	int		j;
+	char	*rows;
+	char	*columns;
 
+	i = 0;
+	while (!ft_isdigit(str[i]) && str[i] != 0)
+		i++;
+	if (str[i] == '\0' || str[i] == '\n')
+		return (-1);
+	j = i;
+	while (ft_isdigit(str[i]))
+		i++;
+	if (!(rows = ft_strsub(str, j, i - j)))
+		return (-1);
+	i++;
+	j = i;
+	while (ft_isdigit(str[i]))
+		i++;
+	if (!(columns = ft_strsub(str, j, i - j)))
+		return (-1);
+	if (board)
+	{
+		game->by = ft_atoi(rows);
+		game->bx = ft_atoi(columns);
+	}
+	else
+	{
+		game->ty = ft_atoi(rows);
+		game->tx = ft_atoi(columns);
+	}
+	ft_liberator(3, &str, &rows, &columns);
+	return (0);
 }
 
-int			read_board(t_game *game)
+int		read_token(t_game *game)
+{
+	char	**token;
+	char	*str;
+
+	if (get_next_line(0, &str) == -1)
+		return (-1);
+	get_dimensions(str, game, 0);
+}
+
+int		read_board(t_game *game)
 {
 	char	**board;
-	char	**token;
 	int		i;
 	int		ret;
 
@@ -36,6 +82,5 @@ int			read_board(t_game *game)
 		if (i == game->y + 1)
 			break ;
 	}
-	if (read_token(game) == -1)
-		return (-1);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,12 +6,19 @@
 /*   By: rkyttala <rkyttala@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 16:08:14 by rkyttala          #+#    #+#             */
-/*   Updated: 2020/09/09 10:25:37 by rkyttala         ###   ########.fr       */
+/*   Updated: 2020/09/09 12:09:18 by rkyttala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
 
+/*
+** by = board rows
+** bx = board columns
+** c = player character ('O' or 'X')
+** ty = token rows
+** tx = token columns
+*/
 static void	init_game(t_game *game)
 {
 	game->by = 0;
@@ -21,6 +28,10 @@ static void	init_game(t_game *game)
 	game->tx = 0;
 }
 
+/*
+** since the only number on the first line that the filler sends to the player
+** is the player number, it's easy to check whether we are 1 or 2; 'O' or 'X'
+*/
 static int	get_player_character(t_game *game)
 {
 	char	*str;
@@ -29,42 +40,22 @@ static int	get_player_character(t_game *game)
 	ret = get_next_line(0, &str);
 	if (ret == -1)
 		return (-1);
-	get_player_character(str, game);
-	free(str);
 	if (ft_strchr(str, '1'))
 		game->c = 'O';
 	else
 		game->c = 'X';
+	free(str);
+	return (0);
 }
 
-static int	get_dimensions(char *str, t_game *game)
-{
-	int		i;
-	int		j;
-	char	*rows;
-	char	*columns;
-
-	i = 0;
-	while (!ft_isdigit(str[i]) && str[i] != 0)
-		i++;
-	if (str[i] == '\0')
-		return (-1);
-	j = i;
-	while (ft_isdigit(str[i]))
-		i++;
-	if (!(rows = ft_strsub(str, j, i - j)))
-		return (-1);
-	i++;
-	j = i;
-	while (ft_isdigit(str[i]))
-		i++;
-	if (!(columns = ft_strsub(str, j, i - j)))
-		return (-1);
-	game->y = ft_atoi(rows);
-	game->x = ft_atoi(columns);
-	ft_liberator(3, &str, &rows, &columns);
-}
-
+/*
+** - initialize game data struct
+** - read first two lines for player character and board dimensions
+** - in a loop, until the end of game:
+**	-- read board situation
+**	-- read given token
+**	-- place token (i.e. calculate position of piece and output its coordinates)
+*/
 int			main(void)
 {
 	t_game	*game;
@@ -77,14 +68,16 @@ int			main(void)
 		return (-1);
 	while (1)
 	{
-		if (game->by = 0)
+		if (game->bx = 0)
 		{
 			if (get_next_line(0, &str) == -1)
 				return (-1);
-			if (get_dimensions(str, game) == -1)
+			if (get_dimensions(str, game, 1) == -1)
 				return (-1);
 		}
 		if (read_board(game) == -1)
+			return (-1);
+		if (read_token(game) == -1)
 			return (-1);
 		place_piece(game);
 	}
